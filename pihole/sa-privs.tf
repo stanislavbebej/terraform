09f2c777836566ab143c554e7c6b5a838f908e7c
Stanislav Bebej <stanislav.bebej@gmail.com>
2024-07-08 22:57:21 +0200

    chore: update PiHole in GCP

---
+++ pihole/sa-privs.tf
@@ -0,0 +1,11 @@
+resource "google_project_iam_member" "logging_log_writer" {
+  project = var.gcp.project
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
+
+resource "google_project_iam_member" "monitoring_metric_writer" {
+  project = var.gcp.project
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
