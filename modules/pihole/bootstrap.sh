12ccc339a725cff72f176ae9bdb158e9d98e1513
Stanislav Bebej <stanislav.bebej@gmail.com>
2024-07-09 20:49:40 +0200

    fix: skip bootstrap after reboot

--- modules/pihole/bootstrap.sh
+++ modules/pihole/bootstrap.sh
@@ -4,8 +4,9 @@
 # sudo journalctl -fu google-startup-scripts.service
 
 # Install PiHole
-mkdir -p /etc/pihole
-cat >/etc/pihole/setupVars.conf <<END
+if ! which pihole &>/dev/null; then
+    mkdir -p /etc/pihole
+    cat >/etc/pihole/setupVars.conf <<END
 PIHOLE_INTERFACE=${NET_INTERFACE}
 PIHOLE_DNS_1=8.8.8.8
 PIHOLE_DNS_2=8.8.4.4
@@ -20,10 +21,12 @@ DNSMASQ_LISTENING=local
 WEBPASSWORD=
 BLOCKING_ENABLED=true
 END
-curl -sSL https://install.pi-hole.net | sudo bash -s -- --unattended
+    curl -sSL https://install.pi-hole.net | sudo bash -s -- --unattended
+fi
 
 # Install WireGuard with PiHole
-cat >/dev/shm/wireguard-setupVars.conf <<END
+if ! which pivpn &>/dev/null; then
+    cat >/dev/shm/wireguard-setupVars.conf <<END
 pivpnforceipv6route=1
 IPv4dev=${NET_INTERFACE}
 install_user=${VM_USER}
@@ -38,7 +41,8 @@ pivpnenableipv6=0
 ALLOWED_IPS="10.77.77.0/24"
 UNATTUPG=1
 END
-curl -sSL https://install.pivpn.io | sudo bash -s -- --unattended /dev/shm/wireguard-setupVars.conf
+    curl -sSL https://install.pivpn.io | sudo bash -s -- --unattended /dev/shm/wireguard-setupVars.conf
+fi
 
 # Install OPS agent
 curl -sSL https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh | sudo bash -s -- --also-install
