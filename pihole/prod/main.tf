2a3c8df47e393e41a18ba4fd51516de7a848fa80
Stanislav Bebej <stanislav.bebej@gmail.com>
2026-02-18 22:54:41 +0100

    chore: update authorized keys

--- pihole/prod/main.tf
+++ pihole/prod/main.tf
@@ -6,7 +6,9 @@ provider "google" {
 
 module "pihole" {
   # checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
-  source = "github.com/stanislavbebej/terraform//modules/pihole?ref=v0.0.0" # x-release-please-version
+  # source = "github.com/stanislavbebej/terraform//modules/pihole?ref=v0.0.0" # x-release-please-version
+
+  source = "github.com/stanislavbebej/terraform//modules/pihole?ref=devel"
 
   environment = "prod"
   gcp_project = "support-infra"
